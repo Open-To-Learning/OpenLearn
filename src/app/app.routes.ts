8de99100ce9e55f936093c96d1ad1bbd9b794b15
Gopal Chaudhary <112993505+GopalChaudh@users.noreply.github.com>
2024-03-28 09:50:57 +0530

    error page created, user-profile created edits in navbar links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './core/pages/home/home.component';
+import { ErrorPageComponent } from './core/pages/error-page/error-page.component';
+import { UserProfileComponent } from './core/pages/user-profile/user-profile.component';
 export const routes: Routes = [
-    {path:'' , component:HomeComponent}
+    {path:'' , component:HomeComponent},
+    {path:'user/:userName' , component:UserProfileComponent},
+    {path:'**', pathMatch:'full' , component:ErrorPageComponent},
 ];
--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,13 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-  logoImage = "/assets/img/logo.png"
+  logoImage = "/assets/img/logo.png";
+  navbarLinks  = [
+    {link:'/',name:'home'},
+    {link:'/explore',name:'Explore'},
+    {link:'/progress',name:'Progress'},
+    {link:'/about',name:'About'},
+    {link:'/docs',name:'Docs'},
+  ]
 
 }
