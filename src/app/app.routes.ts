2fa343ae29f57b04536c85fccae762171321984f
Gopal Chaudhary <112993505+GopalChaudh@users.noreply.github.com>
2024-03-28 23:47:10 +0530

    explore course and specific courses routes added . component sample made

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,14 @@ import { ErrorPageComponent } from './core/pages/error-page/error-page.component
 import { UserProfileComponent } from './core/pages/user-profile/user-profile.component';
 import { LoginComponent } from './core/pages/auth/login/login.component';
 import { SignupComponent } from './core/pages/auth/signup/signup.component';
+import { ExploreComponent } from './core/pages/explore/explore.component';
+import { CourseVisitComponent } from './core/pages/course-visit/course-visit.component';
 export const routes: Routes = [
     {path:'' , component:HomeComponent},
     {path:'login' , component:LoginComponent},
     {path:'signup' , component:SignupComponent},
     {path:'user/:userName' , component:UserProfileComponent},
+    {path:'explore' , component:ExploreComponent},
+    {path:'course/:id' , component:CourseVisitComponent},
     {path:'**', pathMatch:'full' , component:ErrorPageComponent},
 ];
---
+++ src/app/core/pages/course-visit/course-visit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-visit',
+  standalone: true,
+  imports: [],
+  templateUrl: './course-visit.component.html',
+  styleUrl: './course-visit.component.scss'
+})
+export class CourseVisitComponent {
+
+}
