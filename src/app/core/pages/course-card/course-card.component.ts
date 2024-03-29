57ff705b5dcd0e736a4f9e1a3c2c4693e40f5abe
Gopal Chaudhary <112993505+GopalChaudh@users.noreply.github.com>
2024-03-29 09:38:04 +0530

    fix Routing

--- src/app/core/pages/course-card/course-card.component.ts
+++ src/app/core/pages/course-card/course-card.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-course-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.scss'
 })
