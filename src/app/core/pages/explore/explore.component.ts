1ea88ce24d6351bf040470124e58297fedd57675
Gopal Chaudhary <112993505+GopalChaudh@users.noreply.github.com>
2024-03-29 06:56:27 +0530

    course routes are connected course card explore pages fixed

--- src/app/core/pages/explore/explore.component.ts
+++ src/app/core/pages/explore/explore.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CourseCardComponent } from '../course-card/course-card.component';
 
 @Component({
   selector: 'app-explore',
   standalone: true,
-  imports: [],
+  imports: [CourseCardComponent],
   templateUrl: './explore.component.html',
   styleUrl: './explore.component.scss'
 })
