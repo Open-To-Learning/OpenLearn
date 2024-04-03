440ee86c2d1ce0d41aae94fd21d7a90dda4fc01e
Gopal Chaudhary <112993505+GopalChaudh@users.noreply.github.com>
2024-04-03 15:52:57 +0530

    user profile toggle setup

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,29 +1,57 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
-  
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   logoImage = "/assets/img/logo.png";
-  navbarLinks  = [
-    {link:'/',name:'Home'},
-    {link:'/explore',name:'Explore'},
-    {link:'/progress',name:'Progress'},
-    {link:'/about',name:'About'},
-    {link:'/docs',name:'Docs'},
+  isuserLoggedIn = false;
+  navbarLinks = [
+    { link: '/', name: 'Home' },
+    { link: '/explore', name: 'Explore' },
+    { link: '/progress', name: 'Progress' },
+    { link: '/about', name: 'About' },
+    { link: '/docs', name: 'Docs' },
   ];
+  profileFloatingNavigation =[
+    { link: '/user/0', name: 'profile' },
+    { link: '/settings', name: 'settings' },
+  ]
+  toggleprofile = false;
 
   isNavbarOpen = false;
+
+  cookies: string = "";
+
+  // constructor(private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    // Check if document object is available (not in Angular Universal or server-side rendering)
+    if (typeof document !== 'undefined') {
+      
+      this.cookies = document.cookie.split("user=")[1];
+      console.log(this.cookies);
+      if(this.cookies){
+
+        this.isuserLoggedIn = true;
+      }
+      
+    }
+  }
 
   toggleNavbar() {
     this.isNavbarOpen = !this.isNavbarOpen;
   }
+  toggleprofilefunc(){
+    this.toggleprofile = !this.toggleprofile;
+  }
+  logout(){
 
+  }
 }
