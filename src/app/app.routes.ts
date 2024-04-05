1a183770fe340bdf135620ef71dbff5b8614350f
Saurav1872 <saurav.suman1807@gmail.com>
2024-04-05 05:34:09 +0530

    upload video form component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,10 @@ import { LoginComponent } from './core/pages/auth/login/login.component';
 import { SignupComponent } from './core/pages/auth/signup/signup.component';
 import { ExploreComponent } from './core/pages/explore/explore.component';
 import { CourseVisitComponent } from './core/pages/course-visit/course-visit.component';
+import { UploadVideoComponent } from './core/components/upload-video/upload-video.component';
 export const routes: Routes = [
     {path:'' , component:HomeComponent},
+    {path:'upload' , component:UploadVideoComponent},
     {path:'login' , component:LoginComponent},
     {path:'signup' , component:SignupComponent},
     {path:'user/:userName' , component:UserProfileComponent},
